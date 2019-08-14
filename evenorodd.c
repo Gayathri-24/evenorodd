4b0c8a86ced84f7ed3203bf216dce7ba6d115d49
Gayathri-24 <53937901+Gayathri-24@users.noreply.github.com>
2019-08-14 16:00:33 +0530

    Rename evenorodd          to evenorodd.c

---
+++ evenorodd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int num;
+clrscr();
+printf("Enter any number to check even or odd:");
+scanf("%d",&num);
+if(num%2==0)
+printf("\n Number is even");
+else
+printf("\n Number is odd");
+getch();
+}
